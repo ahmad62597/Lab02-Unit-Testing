397c9eca884017b7ad38f8849ff7ddd0abc1dbd0
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-09 13:47:35 -0800

    Analyze options

--- Lab02-Unit-Testing/Program.cs
+++ Lab02-Unit-Testing/Program.cs
@@ -4,24 +4,43 @@ namespace Lab02_Unit_Testing
 {
     class Program
     {
+        public static int UserBalance = 1000;
+
+
         static void Main(string[] args)
-        {
+        {   //Welcome menu
             Console.WriteLine("Hello. Welcome to your bank account. ");
             Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            MainMenu();
             Console.ReadKey();
 
 
         }
 
         static void MainMenu()
-        {
+        {   //Creating menu for user 
+            Console.WriteLine("Select an option: 1, 2 or 3.");
             Console.WriteLine("1.View Balance");
             Console.WriteLine("2.Withdraw Money");
             Console.WriteLine("3.Add Money");
 
-
-
+            //Test userinput via string
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Your input was recieved." + userInput);
 
         }
+
+        static void AnalyzeOptions(string userInput)
+        {
+            if (userInput == "1")
+            {
+                ViewBalance();
+            }
+        }
+
+
+       
     }
 }
