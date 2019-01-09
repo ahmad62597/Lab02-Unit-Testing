063ff412800aa847ac4dfffd0cf0dab60fc21bf1
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-09 13:29:48 -0800

    User Interface

---
+++ Lab02-Unit-Testing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab02_Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello. Welcome to your bank account. ");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("1.View Balance");
+            Console.WriteLine("2.Withdraw Money");
+            Console.WriteLine("3.Add Money");
+
+
+
+
+        }
+    }
+}
